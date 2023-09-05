a25bf5736a96f8910ddedfb135c9871c37bb0107
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-09-05 11:58:08 -0500

    the style was a little sneaky

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const ColoringOf = {color: 'firebrick'}
+  return (
+        <div id="home">
+          <h1 style={ColoringOf}>{name} is a Web Developer from {city}</h1>
+        </div>
+  )
 }
 
 export default Home;
